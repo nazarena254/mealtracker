bc3209b76e8ebbc95966eedf34d16d49adb4611c
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-04-13 20:01:26 +0300

    add the delete button function and sorting calories function

--- src/app/meal-service/meal.service.ts
+++ src/app/meal-service/meal.service.ts
@@ -7,8 +7,9 @@ import { Test } from '../test';
 
 export class MealService {
   meals : Test[]= [
-   
-    new Test('chapobeans',300,'medium calories',),
+    new Test('Biryani',800,'medium calories',),
+    new Test('chapobeans',150,'medium calories',),
+    new Test('chaps&kuku',1200,'medium calories',),
     new Test('ugalibeef',400,'high calories',)  ]
 
   constructor() { }
@@ -19,7 +20,22 @@ export class MealService {
   addMeal(meal:Test){
     this.meals.push(meal)
   }
-
+  // highCalorie(){
+  //   const calorieAboveMedium:any[]=[];
+  //   for(let i=0; i<this.meals.length; i++){
+  //     if(this.meals[i].calories > 500){
+  //       calorieAboveMedium.push(this.meals[i]);
+  //     }
+  //   }
+  // }
+  // lowCalorie(){
+  //   const calorieBelowMedium:any[]=[];
+  //   for(let i=0; i<this.meals.length; i++){
+  //     if(this.meals[i].calories < 500){
+  //       calorieBelowMedium.push(this.meals[i]);
+  //     }
+  //   }
+  // }
   // calories: number;
   // sortMeal(){
   //  for (let calorie of calorie >= 500){
--- src/app/meal/meal.component.ts
+++ src/app/meal/meal.component.ts
@@ -9,6 +9,43 @@ import { Test } from '../test';
   providers:[MealService]
 })
 export class MealComponent implements OnInit {
+  
+  highCalorie(){
+    const calorieAboveMedium:any[]=[];
+    for(let i=0; i<this.meals.length; i++){
+      if(this.meals[i].calories > 500){
+        calorieAboveMedium.push(this.meals[i]);
+      }
+    }
+    this.meals=calorieAboveMedium;
+    alert("View meals with CALORIES above 500");
+  }
+
+  lowCalorie(){
+    const calorieBelowMedium:any[]=[];
+    for(let i=0; i<this.meals.length; i++){
+      if(this.meals[i].calories < 500){
+        calorieBelowMedium.push(this.meals[i]);
+      }
+    }
+    this.meals = calorieBelowMedium;
+    alert("View meals with CALORIES below 500");
+  }
+ 
+  
+  // deleteMeal(meals:any){
+  //   const index: number = this.meals.indexOf(meals);
+  //   if (index !== -1) {
+  //   this.meals.splice(index, 1);
+  //   }
+  // }
+  deleteMeal(value){
+    const index: number = this.meals.indexOf(value);
+    this.meals.splice(index, 1);
+    alert("Are sure you want to delete Meal?");
+  }
+
+
 
   meals: Test[] = [];
   constructor(private mealService:MealService) { this.meals=this.mealService.getMeals(); }
